965464cca979604c0101178d2d93f69fb7ea3336
Khushboo Gupta <realkhushboogupta@gmail.com>
2026-05-31 00:32:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-const endDate = new Date("31 Dec, 2026 23:59:59").getTime();
 const endDate = new Date("1 Jan, 2027 00:00:00").getTime();
-let x=setInterval(function updateTimer(){
-    const now=new Date().getTime();
-    const distanceCovered=now-startDate;
-    const distancePending=endDate-now;
-    //calculate days,mins,hrs,secs
-    const dayINMillis=(24*60*60*1000);
-    const onehourINMillis=(60*60*1000);
-    const secsINMillis=(1000);
-    const days=Math.floor(distancePending/(dayINMillis));
-    const hrs=Math.floor((distancePending%dayINMillis)/onehourINMillis);
-    const mins=Math.floor((distancePending%(onehourINMillis))/(60*1000));
-    const secs=Math.floor((distancePending%(60*1000))/(secsINMillis));
-    //populate in ui
+const startDate = new Date().getTime();
 
-    document.getElementById("days").innerHTML=days;
-    document.getElementById("hours").innerHTML=hrs;
-    document.getElementById("mins").innerHTML=mins;
-    document.getElementById("seconds").innerHTML=secs;
-    //width percentage progress bar
-    const totalDistance=endDate-startDate;
-    const percentageDistance=(distanceCovered/totalDistance)*100;
-    document.getElementById("progress-bar").style.width=percentageDistance + "%";
-    if(distancePending<0){
+let x = setInterval(function updateTimer() {
+    const now = new Date().getTime();
+
+    const distanceCovered = now - startDate;
+    const distancePending = endDate - now;
+
+    const dayINMillis = 24 * 60 * 60 * 1000;
+    const onehourINMillis = 60 * 60 * 1000;
+    const secsINMillis = 1000;
+
+    const days = Math.floor(distancePending / dayINMillis);
+    const hrs = Math.floor((distancePending % dayINMillis) / onehourINMillis);
+    const mins = Math.floor((distancePending % onehourINMillis) / (60 * 1000));
+    const secs = Math.floor((distancePending % (60 * 1000)) / secsINMillis);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hrs;
+    document.getElementById("mins").innerHTML = mins;
+    document.getElementById("seconds").innerHTML = secs;
+
+    const totalDistance = endDate - startDate;
+    const percentageDistance = (distanceCovered / totalDistance) * 100;
+
+    document.getElementById("progress-bar").style.width =
+        percentageDistance + "%";
+
+    if (distancePending < 0) {
         clearInterval(x);
-        document.getElementById("container").innerHTML="EXPIRED";
-        document.getElementById("progress-bar").style.width="100%";
+        document.getElementById("container").innerHTML = "<h1>EXPIRED</h1>";
     }
-},1000);
+}, 1000);
